47cc8d3e8939d8443688f58940aa18b67913d00f
Sriya Sai Sri Vegiraju <99232812+sriyavegiraju@users.noreply.github.com>
2022-06-30 14:06:01 +0530

    Add files via upload

---
+++ e1.js
@@ -0,0 +1,7 @@
+var maths = require("./maths.js")
+var a = 10
+var b  = 5
+console.log("Addition: "+ maths.add(a, b))
+console.log("Subtraction: "+ maths.sub(a, b))
+console.log("Multiplication : "+ maths.mul(a, b))
+console.log("Division: "+ maths.div(a, b))
